95f755a02c22a7c16fddd1f7896fb9711b2b253c
RiadSaidur <reidvai1998@gmail.com>
2023-01-12 11:50:01 +0600

    study plan: day 8

---
+++ leetcode/98. Validate Binary Search Tree.py
@@ -0,0 +1,21 @@
+import collections
+from typing import Optional
+
+
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+def isValidBST(self, root: Optional[TreeNode]) -> bool:
+  def isValid(node, lowerBound, upperBound):
+    if not node:
+      return True
+    
+    if not (node.val < upperBound and node.val > lowerBound):
+      return False
+    
+    return (isValid(node.left, lowerBound, node.val) and isValid(node.right, node.val, upperBound))
+    
+  return isValid(root, float('-inf'), float('inf'))
