38600744822c78ee23995cc4a6d421fcd59a595a
RiadSaidur <reidvai1998@gmail.com>
2023-02-04 12:05:07 +0600

    study plan level 2: day 7

---
+++ leetcode/437. Path Sum III.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+class Solution:
+  def dfs(self, root, targetSum, currPathSum, cache):
+    if root is None:
+      return
+
+    currPathSum += root.val
+    oldPathSum = currPathSum - targetSum
+
+    self.result += cache.get(oldPathSum, 0)
+    
+    cache[currPathSum] = cache.get(currPathSum, 0) + 1
+    
+    self.dfs(root.left, targetSum, currPathSum, cache)
+    self.dfs(root.right, targetSum, currPathSum, cache)
+    
+    cache[currPathSum] -= 1
+
+  def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+    self.result = 0
+    cache = {0:1}
+    
+    self.dfs(root, targetSum, 0, cache)
+    
+    return self.result
