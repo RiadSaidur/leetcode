f3e739856e10577d6738be98faea815f53103a02
RiadSaidur <reidvai1998@gmail.com>
2023-02-06 10:36:00 +0600

    study plan level 2: day 18

---
+++ leetcode/735. Asteroid Collision.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+  stack = []
+
+  for asteroid in asteroids:
+    while stack and asteroid < 0 and asteroids[-1] > 0:
+      diff = asteroid + stack[-1]
+
+      if diff < 0:
+        stack.pop()
+      elif diff > 0:
+        asteroid = 0
+      else:
+        asteroid = 0
+        stack.pop()
+
+    if asteroid:
+      stack.append(asteroid)
+
+  return stack
